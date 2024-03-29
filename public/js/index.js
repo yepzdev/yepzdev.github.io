7ed2d08785474c47c0e6672e7e47f72cd3ad008a
alfredo <alfaradayepez@gmail.com>
2024-03-29 17:59:39 -0500

    fix: The hamburger button does not display in resolutions greater than 768px

--- public/js/index.js
+++ public/js/index.js
@@ -4,13 +4,10 @@ document.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
 });
 
-// HIDE BUTTON 
-//==================================================================================================
-const $width = window.screen.width,
-  $button = document.createElement("button"),
+// Add Hamburger button in the DOM
+// -----------------------------------------------------------
+const $button = document.createElement("button"),
   $container = document.querySelector(".container");
-
-// console.log($width);
 
 $button.className = "panel-btn hamburger hamburger--vortex";
 $button.setAttribute("type", "button");
@@ -19,25 +16,18 @@ $button.innerHTML = `
     <span class="hamburger-inner"></span>
     </span>`;
 
-if ($width <= 768) {
-  $container.appendChild($button);
-}
-//======================================================================================================
+$container.appendChild($button);
 
 // Scrolling header style
-//=======================================================================================================
+// -------------------------------------------------------------------
 document.addEventListener("scroll", () => progress());
 
 function progress() {
   let progress = document.documentElement.scrollTop;
-  // console.log(` Este es el progreso del scrolling ${progress}`);
 
   if (progress >= 40) {
     document.querySelector("header").classList.add("box-shadow");
   } else {
     document.querySelector("header").classList.remove("box-shadow");
   }
-
-  
 }
-//=========================================================================================================
